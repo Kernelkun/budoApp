ba88d5ede50c7a840855df737017e397a5de447e
José Gázquez <jose.j.gazquez@gmail.com>
2018-03-11 19:25:17 +0100

    bug: Uncomment page imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-// import { ListPage } from '../pages/list/list';
-// import { TabsPage } from '../pages/tabs/tabs';
-// import { BeltsPage } from '../pages/belts/belts';
+import { ListPage } from '../pages/list/list';
+import { TabsPage } from '../pages/tabs/tabs';
+import { BeltsPage } from '../pages/belts/belts';
 
 @Component({
   templateUrl: 'app.html'
