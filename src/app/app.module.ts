9158df2f0a3a6e5a05d4c82016d8383145370287
José Gázquez <jose.j.gazquez@gmail.com>
2018-03-15 20:45:55 +0100

    feat: Add basic filter modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 // import { SearchPage } from '../pages/search/search';
 import { BeltsPage } from '../pages/belts/belts';
 import { TechniquePage } from '../pages/technique/technique';
+import { FilterPage } from '../pages/filter/filter';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -28,6 +29,7 @@ import { SortPipe } from '../pipes/sort/sort';
     BeltsPage,
     TechniquePage,
     SearchPipe,
+    FilterPage,
     SortPipe
   ],
   imports: [
@@ -42,6 +44,7 @@ import { SortPipe } from '../pipes/sort/sort';
     ListPage,
     TabsPage,
     // SearchPage,
+    FilterPage,
     BeltsPage,
     TechniquePage
   ],
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { TechniquePage } from '../technique/technique';
 import { TechniqueProvider } from '../../providers/technique/technique';
+import { ModalController } from 'ionic-angular';
+import { FilterPage } from '../filter/filter';
 
 @Component({
   selector: 'page-list',
@@ -17,7 +19,7 @@ export class ListPage {
   order: number = 1;
   column: string = 'name';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public _technique: TechniqueProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public _technique: TechniqueProvider, public modalCtrl: ModalController) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
@@ -43,4 +45,11 @@ export class ListPage {
   onCancel(event) {
     console.log(event);
   }
+
+  public openModal() {
+    console.log("MEOW");
+    var data = { message: 'hello world' };
+    var modalPage = this.modalCtrl.create(FilterPage, data);
+    modalPage.present();
+  }  
 }
