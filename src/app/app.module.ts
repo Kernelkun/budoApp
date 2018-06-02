620228d96471306ea6e6b6d02809818110b1832e
José Gázquez <jose.j.gazquez@gmail.com>
2018-06-02 11:52:37 +0200

    add memorize page to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
+// Pages
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import { TabsPage } from '../pages/tabs/tabs';
-// import { SearchPage } from '../pages/search/search';
 import { BeltsPage } from '../pages/belts/belts';
 import { TechniquePage } from '../pages/technique/technique';
 import { FilterPage } from '../pages/filter/filter';
-
+import { MemorizePage } from '../pages/memorize/memorize'
+// Miscelanea
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+// Providers
+import { FilterDataProvider } from '../providers/filter-data/filter-data';
 import { TechniqueProvider } from '../providers/technique/technique';
 import { DataProvider } from '../providers/data/data';
+// Pipes
 import { SearchPipe } from '../pipes/search/search';
 import { SortPipe } from '../pipes/sort/sort';
 import { BeltPipe } from '../pipes/belt/belt';
 import { SafeUrlPipe } from '../pipes/safe-url/safe-url';
 import { ReversePipe } from '../pipes/reverse/reverse';
-import { FilterDataProvider } from '../providers/filter-data/filter-data';
 
 /* Firebase */
 import { AngularFireModule } from 'angularfire2';
@@ -42,7 +44,6 @@ const firebaseConfig = {
     HomePage,
     ListPage,
     TabsPage,
-    // SearchPage,
     BeltsPage,
     TechniquePage,
     SearchPipe,
@@ -50,7 +51,8 @@ const firebaseConfig = {
     SortPipe,
     BeltPipe,
     SafeUrlPipe,
-    ReversePipe
+    ReversePipe,
+    MemorizePage
   ],
   imports: [
     BrowserModule,
@@ -65,10 +67,10 @@ const firebaseConfig = {
     HomePage,
     ListPage,
     TabsPage,
-    // SearchPage,
     FilterPage,
     BeltsPage,
-    TechniquePage
+    TechniquePage,
+    MemorizePage
   ],
   providers: [
     StatusBar,
